d2bc70ace98d6765f821a3cce0b3311943ed70b7
ceccs17d51 <35443067+ceccs17d51@users.noreply.github.com>
2018-11-30 18:34:42 +0530

    Update dequeue_array.c

--- dequeue_array.c
+++ dequeue_array.c
@@ -1,123 +1,100 @@
-//Surya Merin Philip
-//S3D
-//51
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-struct node
-{
-    int data;
-    struct node *link;
-};
+int queue[100], n=100, front=-1, rear=-1;
 
-void main()
-{
-    struct node *head,*temp,*tem;
-    int cho,ele;
-    head=(struct node*)malloc(sizeof(struct node));
-    head->data=NULL;
-    head->link=NULL;
-    do
-    {
-        printf("\nMENU\n1.Insertion\n2.Deletion\n3.Display\nEnter choice: ");
-        scanf("%d",&cho);
-
-        switch(cho)
-        {
-            case 1: printf("Enter element: ");
-                    scanf("%d",&ele);
-                    tem=head;
-                    while(tem->link!=NULL)
-                    {
-                        tem=tem->link;
-                    }
-                    temp=(struct node*)malloc(sizeof(struct node));
-                    temp->link=NULL;
-                    tem->link=temp;
-                    temp->data=ele;
-                    break;
-            case 2: if(head->link==NULL)
-                    {
-                        printf("\nQueue is EMPTY\n");
-                    }
-                    else
-                    {
-                        tem=head->link;
-                        temp=tem->link;
-                        head->link=temp;
-                        printf("\nDELETED %d ",tem->data);
-                        free(tem);
-                    }
-                    break;
-            case 3: if(head->link==NULL)
-                    {
-                        printf("\nQueue is EMPTY\n");
-                    }
-                    else
-                    {
-                        temp=head->link;
-                        while(temp!=NULL)
-                        {
-                            printf("%d <-",temp->data);
-                            temp=temp->link;
-                        }
-                        printf(" NULL");
-                    }
-                    break;
-        }
-    }while(cho==1 || cho==2 || cho==3);
+void enqueueRear() {
+	int data;
+	if (rear == n-1) {
+		printf("Failed: Queue full\n");
+	} else {
+		printf("Enter data to be entered : ");
+		scanf("%d", &data);
+		if (front == -1) {
+			front = rear = 0;
+		} else {
+			rear++;
+		}
+		queue[rear] = data;
+	}
 }
-OUTPUT
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 2
-
-Queue is EMPTY
-
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 1
-Enter element: 3
-
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 1
-Enter element: 8
-
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 3
-3 <-8 <- NULL
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 2
-
-DELETED 3 
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 1
-Enter element: 9
-
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 3
-8 <-9 <- NULL
-MENU
-1.Insertion
-2.Deletion
-3.Display
-Enter choice: 8
+void enqueueFront() {
+    int data;
+    if (front == 0) {
+        printf("Failed: No space at front\n");
+    } else {
+        printf("Enter data to be inserted : ");
+        scanf("%d", &data);
+        if (front == -1) {
+            front = rear = 0;
+        } else {
+            front--;
+        }
+        queue[front] = data;
+    }
+}
+void dequeueRear() {
+    if (rear == -1) {
+        printf("Failed: Empty queue\n");
+    } else {
+        printf("Deleted element : %d\n", queue[rear]);
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            rear--;
+        }
+    }
+}
+void dequeueFront() {
+	if (front == -1) {
+		printf("Failed: Empty queue\n");
+	} else {
+		printf("Deleted element : %d\n", queue[front]);
+		if (front == rear) {
+			front = rear = -1;
+		} else {
+			front++;
+		}
+	}
+}
+void printQueue() {
+	int i=0;
+	if (front == -1) {
+		printf("Empty Queue\n");
+	} else {
+		for (i=front; i<=rear; i++) {
+			printf("%d\n", queue[i]);
+		}
+	}
+}
+int main() {
+	char choice;
+	front = rear = -1;
+	do {
+		printf("\n\tQueue using array\n");
+        printf("1.Enqueue Front\n");
+        printf("2.Enqueue Rear\n");
+        printf("3.Dequeue Front\n");
+        printf("4.Dequeue Rear\n");
+        printf("5.Display\n");
+        printf("6.Exit\n");
+        printf("Enter your choice : ");
+		scanf(" %c", &choice);
+		switch(choice) {
+			case '1':
+				enqueueFront();
+				break;
+			case '2':
+				enqueueRear();
+				break;
+			case '3':
+				dequeueFront();
+				break;
+			case '4':
+				dequeueRear();
+				break;
+			case '5':
+				printQueue();
+				break;
+		}
+	} while(choice != '6');
+}
